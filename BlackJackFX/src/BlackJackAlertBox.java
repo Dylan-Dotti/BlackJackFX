3cf552f5fb631fbdacdc8531c69c24a33dc6b918
Dylan Dotti <invocation42@yahoo.com>
2019-08-28 01:59:45 -0400

    Added alert boxes.

---
+++ BlackJackFX/src/BlackJackAlertBox.java
@@ -0,0 +1,32 @@
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+public class BlackJackAlertBox {
+	public static void display(String title, String message,
+			Runnable onExitAction) {
+		Stage window = new Stage();
+		window.initModality(Modality.APPLICATION_MODAL);
+		window.setTitle(title);
+		window.setMinWidth(250);
+		
+		Label messageLabel = new Label(message);
+		Button confirmButton = new Button("OK");
+		confirmButton.setOnAction(e -> {
+			window.close();
+			onExitAction.run();
+		});
+		
+		VBox root = new VBox(10);
+		root.getChildren().addAll(messageLabel, confirmButton);
+		root.setAlignment(Pos.CENTER);
+		
+		Scene scene = new Scene(root);
+		window.setScene(scene);
+		window.show();
+	}
+}
